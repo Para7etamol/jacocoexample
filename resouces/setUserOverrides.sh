f8b33be3f86408806f4ebf3d9836bef5d54ac504
Oliver Stuch <o.stuch@verband.creditreform.de>
2018-02-13 20:13:45 +0100

    provided jacocoagent version 0.8.0

--- resouces/setUserOverrides.sh
+++ resouces/setUserOverrides.sh
@@ -1,4 +1,5 @@
-# @Sarath: maybe you have to drop -Djava.security.egd=file:/dev/./urandom
-export EXTRA_JAVA_PROPERTIES="-Djava.security.egd=file:/dev/./urandom -javaagent:lib/jacocoagent.jar=output=tcpserver,address=*,port=6301,includes=com.example.jacoco.*,jmx=true"
+# THIS IS ONLY A STARTING POINT !!!
+# @Sarath/Kannan: maybe you have to drop -Djava.security.egd=file:/dev/./urandom
+export EXTRA_JAVA_PROPERTIES="-Djava.security.egd=file:/dev/./urandom -javaagent:lib/org.jacoco.agent-0.8.0-runtime.jar=output=tcpserver,address=*,port=6301,includes=com.example.jacoco.*,jmx=true"
 export debugFlag="true"
 export CLASS_CACHE="true"
