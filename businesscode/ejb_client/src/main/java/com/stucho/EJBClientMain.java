5ee09192e1755599ce09fd925a2fcd6316f472e6
sarathkumarpk <sarathkumarpk@gmail.com>
2018-02-15 19:45:27 +0530

    The ejb client is modified to consume new multiply divide module.

--- businesscode/ejb_client/src/main/java/com/stucho/EJBClientMain.java
+++ businesscode/ejb_client/src/main/java/com/stucho/EJBClientMain.java
@@ -7,6 +7,8 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import com.stucho.add_service.AddBeanRemote;
+import com.stucho.divide_service.DivideBeanRemote;
+import com.stucho.multiply_service.MultiplyBeanRemote;
 import com.stucho.substract_service.SubstractBeanRemote;
 
 /**
@@ -34,11 +36,20 @@ public class EJBClientMain {
 
 			InitialContext ic = getInitialContext("t3://localhost:7001");
 
+			final int value1 = 36;
+			final int value2 = 9;
+			
 			AddBeanRemote obj1 = (AddBeanRemote) ic.lookup("addBean#com.stucho.add_service.AddBeanRemote");
-			System.out.println("The sum is : " + obj1.add(31, 17));
+			System.out.println("The sum of ("+value1+" , "+value2+") is : " + obj1.add(value1, value2));
 			
 			SubstractBeanRemote ob2 = (SubstractBeanRemote) ic.lookup("substractBean#com.stucho.substract_service.SubstractBeanRemote");
-			System.out.println("The difference is : " + ob2.substract(56, 12));
+			System.out.println("The difference of ("+value1+" , "+value2+") is : " + ob2.substract(value1, value2));
+			
+			DivideBeanRemote obj3 = (DivideBeanRemote) ic.lookup("divideBean#com.stucho.divide_service.DivideBeanRemote");
+			System.out.println("The division result of ("+value1+" , "+value2+") is : " + obj3.divide(value1, value2));
+			
+			MultiplyBeanRemote obj4 = (MultiplyBeanRemote) ic.lookup("multiplyBean#com.stucho.multiply_service.MultiplyBeanRemote");
+			System.out.println("The product of ("+value1+" , "+value2+") is : " + obj4.multiply(value1, value2));
 			
 		} catch (final Throwable t) {
 			
