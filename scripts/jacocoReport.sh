ee05f216b93441ef95764a42e4b0b62942552f3b
sarathkumarpk <sarathkumarpk@gmail.com>
2018-03-26 14:47:34 +0530

    antrun plugin execution added to copy artifacts to pick up location. the script file to run the jacoco cli tasks.

---
+++ scripts/jacocoReport.sh
@@ -0,0 +1,17 @@
+SEARCH_LOCATION=/drives/c/Users/sarath.kumar/output
+JACOCO_CLI_HOME=/drives/c/Users/sarath.kumar/java/lib/jacoco-0.8.1-20180212.125243-11/lib
+CLASSES_FOLDER=classes
+SOURCES_FOLDER=sources
+EXEC_DATA_FOLDER=exec_data
+REPORT_FOLDER=site
+EXEC_FILES_SEPARATOR=" "
+java -jar $(cygpath -aw $JACOCO_CLI_HOME/jacococli.jar) dump --address localhost --port 6301 --destfile $(cygpath -aw $SEARCH_LOCATION/$EXEC_DATA_FOLDER/jacoco_weblogic.exec)
+for entry in $SEARCH_LOCATION/$EXEC_DATA_FOLDER/*.exec
+do	
+  echo $entry
+  execFileList+=$(cygpath -aw $entry)$EXEC_FILES_SEPARATOR
+done
+rm -rf $SEARCH_LOCATION/$REPORT_FOLDER
+java -jar $(cygpath -aw $JACOCO_CLI_HOME/jacococli.jar) \
+report $execFileList --classfiles $(cygpath -aw $SEARCH_LOCATION/$CLASSES_FOLDER) \
+--html $(cygpath -aw $SEARCH_LOCATION/$REPORT_FOLDER) --sourcefiles $(cygpath -aw $SEARCH_LOCATION/SOURCES_FOLDER)
